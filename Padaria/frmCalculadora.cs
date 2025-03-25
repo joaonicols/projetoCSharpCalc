a0aa819092f312de890bc0cd19bf59f0d4cd9e66
joão <joaonsarruda@gmail.com>
2025-03-25 09:00:54 -0300

    first commit

---
+++ Padaria/frmCalculadora.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Padaria
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //declarando variáveis
+            //tipo da variável e o nome da variável
+            int valor1, valor4, resp;
+            double valor2, valor5;
+            float valor3;
+            bool flag;
+            string nome;
+            char sexo;
+
+            //inicializar as variáveis
+            valor1 = 10;
+            valor4 = 10;
+
+            resp = valor1 + valor4;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //Limpar os campos
+            txtNumero1.Text = "";
+            txtNumero2.Clear();
+            txtResposta.Clear();
+
+            rdbSomar.Checked = false;
+            rdbSubtrair.Checked = false;
+            rdbMultiplicar.Checked = false;
+            rdbDivisao.Checked = false;
+
+            txtNumero1.Focus();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            //sair do sistema e encerrar todos os processos
+            Application.Exit();
+        }
+    }
+}
