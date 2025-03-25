bf0e3dbc6878d2f172051bdb3ce29b5f50dc3328
joão <joaonsarruda@gmail.com>
2025-03-25 11:57:46 -0300

    novo commit

--- Padaria/frmCalculadora.cs
+++ Padaria/frmCalculadora.cs
@@ -16,26 +16,76 @@ namespace Padaria
         {
             InitializeComponent();
         }
-        
+
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            //declarando variáveis
-            //tipo da variável e o nome da variável
-            int valor1, valor4, resp;
-            double valor2, valor5;
-            float valor3;
-            bool flag;
-            string nome;
-            char sexo;
+            ////declarando variáveis
+            ////tipo da variável e o nome da variável
+            //int valor1, valor4, resp;
+            //double valor2, valor5;
+            //float valor3;
+            //bool flag;
+            //string nome;
+            //char sexo;
 
-            //inicializar as variáveis
-            valor1 = 10;
-            valor4 = 10;
+            ////inicializar as variáveis
+            //valor1 = 10;
+            //valor4 = 10;
 
-            resp = valor1 + valor4;
-        }
+            //resp = valor1 + valor4;
 
+            //declarando variaveis
+            double num1, num2, resp = 0;
+
+            try
+            {
+
+                //inicializar as variaveis
+                num1 = Convert.ToDouble(txtNumero1.Text);
+                num2 = Convert.ToDouble(txtNumero2.Text);
+
+
+                if (rdbSomar.Checked)
+                {
+                    resp = num1 + num2;
+
+                }
+                if (rdbSubtrair.Checked)
+                {
+                    resp = num1 - num2;
+
+                }
+                if (rdbMultiplicar.Checked)
+                {
+                    resp = num1 * num2;
+
+                }
+                if (rdbDivisao.Checked)
+                {
+                    if (num2 == 0)
+                    {
+                        MessageBox.Show("Impossível dividir por zero!",
+                            "Mensagem do sistema",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error,
+                            MessageBoxDefaultButton.Button1);
+                        resp = 0;
+                    }
+                    else
+                    {
+                        resp = num1 / num2;
+                    }
+
+                }
+
+                txtResposta.Text = resp.ToString();
+            } catch (Exception)
+            {
+
+            }
+            }
+            
         private void btnLimpar_Click(object sender, EventArgs e)
         {
             //Limpar os campos
